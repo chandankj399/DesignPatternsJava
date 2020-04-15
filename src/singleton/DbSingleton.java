6b4eca92232018a507d3384f4464578d8d951400
chandankj399 <chandankj399@gmail.com>
2020-04-15 11:17:14 -0700

    Added derby db to demonstrate singleton

--- src/singleton/DbSingleton.java
+++ src/singleton/DbSingleton.java
@@ -1,10 +1,21 @@
 package singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
 
 	private static DbSingleton instance = null;
+	private static volatile Connection conn = null;
 
 	private DbSingleton() {
+		
+		try {
+			DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		if (instance != null) {
 			throw new RuntimeException("Use getInstance() method to create instance");
 		}
@@ -20,4 +31,23 @@ public class DbSingleton {
 		}
 		return instance;
 	}
+	
+	
+	
+	public Connection getConnection() {
+		if(conn == null) {
+			synchronized (DbSingleton.class) {
+				if(conn == null) {
+					try {
+						String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+						conn = DriverManager.getConnection(dbUrl);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return conn;
+	}
+	
 }
--- src/singleton/DbSingletonDemo.java
+++ src/singleton/DbSingletonDemo.java
@@ -1,15 +1,37 @@
 package singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonDemo {
 
 	public static void main(String[] args) {
 		DbSingleton instDbSingleton = DbSingleton.getInstance();
 		
-		System.out.println(instDbSingleton);
+		long timeBefore = 0;
+		long timeAfter = 0;
 		
-		DbSingleton anotherInstance = DbSingleton.getInstance();
+		timeBefore = System.currentTimeMillis();
+		Connection conn = instDbSingleton.getConnection();
+		timeAfter = System.currentTimeMillis();
+		System.out.println(timeAfter - timeBefore);
 		
-		System.out.println(anotherInstance);
+		Statement sta ;
+		try {
+			sta = conn.createStatement();
+			int count = sta.executeUpdate("CREATE TABLE Address (ID INT, StreetName VARCHAR(20), City VARCHAR(20))");
+			System.out.println("Table Created");
+			sta.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		timeBefore = System.currentTimeMillis();
+		conn = instDbSingleton.getConnection();
+		timeAfter = System.currentTimeMillis();
+		System.out.println(timeAfter - timeBefore);
+		
 	}
+	
 
 }
