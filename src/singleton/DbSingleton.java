f6086811193e679c200596369028ddee13524df9
chandankj399 <chandankj399@gmail.com>
2020-04-15 10:51:30 -0700

    Made singleton thread safe

--- src/singleton/DbSingleton.java
+++ src/singleton/DbSingleton.java
@@ -1,14 +1,22 @@
 package singleton;
 
 public class DbSingleton {
-	
+
 	private static DbSingleton instance = null;
-	
-	private DbSingleton() {}
-	
+
+	private DbSingleton() {
+		if (instance != null) {
+			throw new RuntimeException("Use getInstance() method to create instance");
+		}
+	}
+
 	public static DbSingleton getInstance() {
-		if(instance == null) {
-			instance = new DbSingleton();     // Lazy Loading
+		if (instance == null) {
+			synchronized (DbSingleton.class) {      // for thread safety
+				if (instance == null) {
+					instance = new DbSingleton(); // Lazy Loading
+				}
+			}
 		}
 		return instance;
 	}
